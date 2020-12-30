a4f8592b561efcfef6b1b22afd02f62079f26119
DoeyJonuts <joeyefarah@gmail.com>
2020-12-30 17:38:05 -0600

    Update main.js

--- main.js
+++ main.js
@@ -106,6 +106,7 @@ function displayButton(element) {
     for (i = 0; i < a.length; i++) {
       txtValue = a[i].textContent || a[i].innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        
         a[i].style.display = "";
       } else {
         a[i].style.display = "none";
